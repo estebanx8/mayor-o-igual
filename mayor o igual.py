d29a78fd33de78be17635e44be5a0f26d2fcd8d8
estebanx8 <160289586+estebanx8@users.noreply.github.com>
2024-02-29 19:43:09 -0500

    edades.py
    codigo para diferenciar que persona es mayor a la otra

---
+++ mayor o igual.py
@@ -0,0 +1,9 @@
+p1=input("digite su nombre=")
+edad1=int (input("digite su edad="))
+p2=input("digite su nombre=")
+edad2=int(input("digite su edad="))
+#resultado
+if (edad1 > edad2):
+    print("la persona mayor es ", p1)
+else:
+    print("la persona mayor es ", p2)
